c66d7566ca60efce6f92b465852a5c95a22c8313
mickpav <mickpav@gmail.com>
2023-07-29 20:52:09 +1000

    terraform-1

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "y0_AgAAAAAB_SyYAATuwQAAAADkv4kzeqV86T0ZR1WBbmkH9RznwY__0E0"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+resource "yandex_compute_instance" "app" {
+  count = var.vm_count
+  name  = "reddit-app-${count.index}"
+
+  metadata = {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+  }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+}
