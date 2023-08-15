502842c6f6f3a802a40a45ad27c98120516fd052
mickpav <mickpav@gmail.com>
2023-08-15 14:00:06 +1000

    fix #10

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -16,8 +16,11 @@ provider "yandex" {
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
+  ssh_user        = var.ssh_user
+  db_url          = var.db_url
 }
 
 module "db" {
